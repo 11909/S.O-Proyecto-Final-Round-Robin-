347c8ae9ae55c96af7a479b437a534cd9634e5f8
HecRod56 <rodriguez.guarneros.hector.daniel@gmail.com>
2025-11-27 23:35:45 -0600

    Estructura, declaración y definición de la estructura cola (queue)

---
+++ queue.c
@@ -0,0 +1,289 @@
+/*
+IMPLEMENTACION DE LA LIBRERIA DEL TAD COLA DINÁMICA (TADColaEst.h)
+AUTOR: Edgardo Adrián Franco Martínez (C) Noviembre 2022
+IMPLEMENTA: Rodriguez Guarneros Hector Daniel 
+VERSIÓN: 1.0 Mayo 2024
+
+DESCRIPCIÓN: TAD cola o Queue.
+Estructura de datos en la que se cumple:
+Los elementos se insertan en un extremo (el posterior) y 
+la supresiones tienen lugar en el otro extremo (frente).
+
+OBSERVACIONES: Hablamos de una Estructura de datos dinámica 
+cuando se le asigna memoria a medida que es necesitada, 
+durante la ejecución del programa. 
+
+
+COMPILACIÓN PARA GENERAR EL CÓDIGO OBJETO: gcc -c TADColaEst.c
+*/
+
+//LIBRERAS
+#include "queue.h"
+#include <stdlib.h>
+#include <stdio.h>
+//DEFINICIÓN DE FUNCIONES
+
+/*
+void Initialize(cola *c);
+Descripción: Inicializar cola (Iniciar una cola para su uso)
+Recibe: cola *c (Referencia a la cola "c" a operar)
+Devuelve:
+Observaciones: El usuario a creado una cola y c tiene la referencia a ella, 
+si esto no ha pasado se ocasionara un error.
+*/
+void Initialize(cola * c)
+{
+	c->num_elem=0;
+	c->frente=NULL;
+	c->final=NULL;
+	
+
+	return;
+}
+
+/*
+void Queue(cola * c, elemento e);
+Descripción: Recibe una cola y agrega un elemento al final de ella.
+Recibe: cola *c (Referencia a la cola "c" a operar) elemento e (Elemento a encolar)
+Devuelve:
+Observaciones: El usuario a creado una cola y ha sido inicializada y c tiene la referencia a ella, 
+si esto no ha pasado se ocasionara un error.
+*/
+void Queue(cola * c, elemento e)
+{
+	nodo* aux;
+	aux= malloc(sizeof(nodo));
+	if (aux==NULL){
+		printf("ERROR: Queue (c,e) desbordamiento de cola");
+		exit(1);
+	}
+	aux->e = e;
+	aux->siguiente = NULL;
+	if (c->num_elem > 0)
+		c->final->siguiente = aux;
+	c->final = aux;
+	c->num_elem++;
+	if (c->num_elem == 1)
+		c->frente = c->final;
+	return;
+}
+
+/*
+elemento Dequeue(cola * c);
+Descripción: Recibe una cola y devuelve el elemento que se encuentra al 
+frente de esta, quitándolo de la cola.
+Recibe: cola *c (Referencia a la cola "c" a operar)
+Devuelve: elemento (Elemento desencolado)
+Observaciones: El usuario a creado e inicializado una cola, ademas la cola tiene 
+elementos, si no es así se genera un error y se termina el programa. (Subdesboramiento de Cola)
+*/
+elemento Dequeue(cola * c)
+{
+	nodo *aux;
+	elemento e;//Elemento a retornar 
+	if(c->num_elem==0)
+	{
+		printf("ERROR: Dequeue (c) subdesbordamiento de cola");
+		exit(1);
+	}
+	else 
+	{
+		e=c->frente->e;
+		aux=c->frente;
+		c->frente=c->frente->siguiente;
+		free(aux);
+		c->num_elem--;
+		if(c->num_elem==0)
+			c->final=NULL;
+	}
+
+	//Retornar al elemento
+	return e;
+}
+
+/*
+boolean Empty(cola * c);
+Descripción: Recibe una cola y TRUE si la cola esta vacia y FALSE en caso contrario
+Recibe: cola *c (Referencia a la cola "c" a verificar)
+Devuelve: boolean TRUE O FALSE
+Observaciones: El usuario a creado una cola y la cola fue correctamente inicializada
+*/
+boolean Empty(cola * c)
+{
+	
+	return (c->num_elem==0)?TRUE:FALSE;
+}
+
+/*
+elemento Front(cola * c);
+Descripción: Recibe una cola y devuelve el elemento que se encuentra al frente de esta.
+Recibe: cola *c (Referencia a la cola "c")
+Devuelve: elemento del frente de la cola
+Observaciones: El usuario a creado una cola,la cola fue correctamente inicializada, esta 
+tiene elementos de lo contrario devolvera ERROR. *No se modifica el TAD
+*/
+elemento Front(cola * c)
+{
+	elemento e;
+	
+	e=c->frente->e;
+	
+	return e;
+}
+
+/*
+elemento Final(cola * c);
+Descripción: Recibe una cola y devuelve el elemento que se encuentra al final de esta.
+Recibe: cola *c (Referencia a la cola "c")
+Devuelve: elemento del final de la cola
+Observaciones: El usuario a creado una cola,la cola fue correctamente inicializada, esta 
+tiene elementos de lo contrario devolvera ERROR. *No se modifica el TAD
+*/
+elemento Final(cola * c)
+{
+	elemento e;	
+	e=c->final->e;
+	return e;
+}
+
+/*
+int Size(cola * c);
+Descripción: Recibe una cola y devuelve el número de elemento que se encuentran en esta.
+Recibe: cola *c (Referencia a la cola "c")
+Devuelve: int (Tamaño de la cola)
+Observaciones: El usuario a creado una cola,la cola fue correctamente inicializada, esta 
+*No se modifica el TAD
+*/
+int Size(cola * c)
+{
+	int r;
+	r=c->num_elem;	
+	return r;
+}
+
+/*
+void Element(cola * c, int i);
+Descripción: Recibe una cola y un número de elemento de 1 al tamaño de la cola y retorna el elemento de esa posición.
+Devuelve: elemento en la posicion i de la cola
+Observaciones: El número i debera estar entre 1 y el tamaño de la cola, si esta es vacia o mas pequeña se provoca un error.
+*/
+elemento Element(cola * c, int i)
+{
+	elemento r;
+	nodo *aux;
+	int j;
+	
+	if(i>c->num_elem || i<=0)
+	{
+		printf("ERROR: Element(c,i) i se encuentra fuera del rango");
+		exit(1);
+	}
+	aux = c->frente;
+
+	for(j=1;j<i;j++)
+	{
+		aux = aux->siguiente;
+	}
+	r = aux->e;
+
+	return r;
+}
+
+/*
+void Destroy(cola * c);
+Descripción: Recibe una cola y la libera completamente.
+Recibe: cola *c (Referencia a la cola "c" a operar)
+Devuelve:
+Observaciones: La cola se destruye y se inicializa.
+*/
+void Destroy(cola * c)
+{
+	nodo *aux;
+	
+	aux = c->frente;
+	while (aux!=NULL)
+	{
+		c->frente = aux->siguiente;
+		free(aux);
+		aux = c->frente;
+	}
+	c->num_elem = 0;
+	c->frente = NULL;
+	c->final = NULL;
+	
+	return;
+}
+
+/*
+int Search(cola *c, elemento e);
+Descripción: Recibe una cola y un elemento, busca el elemento
+			 en la cola, y retorna su posicion en la cola.
+Recibe: cola *c (Referencia a la cola "c" donde buscar)
+		elemento e (Elemento a buscar en la cola "c")
+Devuelve: Posicion del elemento "e" en la cola "c"
+Observaciones: En caso de que el elemento "e" no exista
+			   en la cola "c", se retorna el valor 0.
+*/
+int Search(cola * c, elemento e){
+	int pos = 0;
+	elemento aux_e;
+	nodo *aux_n;
+	int i, n;
+	
+	if(Empty(c)){
+		printf("Error: Search(c,e) la pila esta vacia");
+		exit(1);
+	}
+		
+	n = Size(c);
+	
+	aux_n = c -> frente;
+	
+	for(i = 1; i <= n; i++){
+		aux_e = aux_n -> e;
+		if(aux_e.c == e.c && aux_e.n == e.n && aux_e.f == e.f){
+			pos = i;
+			break;
+		}
+		aux_n = aux_n->siguiente;
+	}
+	
+	return pos;
+}
+
+/*
+void Flip(cola *c);
+Descripción: Recibe una cola y voltea el orden de los elementos
+			 en la cola.
+Recibe: cola *c (Referencia a la cola "c" a operar)
+Devuelve:
+Observaciones: El elemento al frente de la cola, terminara al final
+			   de la cola, y viceversa para el elemento al final.
+*/
+void Flip(cola *c){
+	cola aux_c;
+	elemento aux_e;
+	int i, s, n;
+	
+	Initialize(&aux_c);
+	if(Empty(c)){
+		printf("Error: Flip(c) la pila esta vacia");
+		exit(1);
+	}
+	
+	n = Size(c);
+	
+	for(i = 1; i <= n; i++){
+		aux_e = Dequeue(c);
+		Queue(&aux_c, aux_e);
+	}
+	
+	s = Size(&aux_c);
+	
+	for(i = s; i > 0; i--){
+		aux_e = Element(&aux_c, i);
+		Queue(c, aux_e);
+	}
+	
+	return;
+}
---
+++ queue.h
@@ -0,0 +1,64 @@
+/*
+LIBRERIA: Cabecera de el TAD COLA DINÁMICA
+AUTOR: Edgardo Adrián Franco Martínez (C) Noviembre 2022
+IMPLEMENTA: Rodriguez Guarneros Hector Daniel 
+VERSIÓN: 1.0 Mayo 2024
+
+DESCRIPCIÓN: TAD cola o Queue.
+Estructura de datos en la que se cumple:
+Los elementos se insertan en un extremo (el posterior) y 
+la supresiones tienen lugar en el otro extremo (frente).
+
+OBSERVACIONES: Hablamos de una Estructura de datos dinámica 
+cuando se le asigna memoria a medida que es necesitada, 
+durante la ejecución del programa. 
+ 
+*/
+
+//DEFINICIONES DE CONSTANTES
+#define TRUE	1
+#define FALSE	0
+
+//DEFINICIONES DE TIPOS DE DATO
+
+//Definir un boolean (Se modela con un "char")
+typedef unsigned char boolean;
+
+//Definir un elemento (Se modela con una estructura "elemento")
+typedef struct elemento
+{
+	char c;
+	int n;
+	float f;
+} elemento;
+
+//Definir la estructura de un nodo dinámico
+typedef struct nodo
+{
+	elemento e;
+	struct nodo *siguiente;
+}nodo;
+
+//Definir una cola 
+typedef struct cola
+{
+	int num_elem;
+	nodo *frente;
+	nodo *final;
+} cola;
+
+//DECLARACIÓN DE FUNCIONES
+void Initialize(cola * c);			//Inicializar cola (Initialize): Recibe una cola y la inicializa para su trabajo normal.
+void Queue(cola * c, elemento e);	//Encolar (Queue): Recibe una cola y agrega un elemento al final de ella. 
+elemento Dequeue(cola * c);			//Desencolar (Dequeue): Recibe una cola y remueve el elemento del frente retornándolo.
+boolean Empty(cola * c);			//Es vacía (Empty): Recibe la cola y devuelve verdadero si esta esta vacía.
+elemento Front(cola * c);			//Frente (Front): Recibe una cola y retorna el elemento del frente.	
+elemento Final(cola * c);			//Final (Final): Recibe una cola y retorna el elemento del final.
+elemento Element(cola *c, int i); 	// Recibe una cola y un número de elemento de 1 al tamaño de la cola y retorna el elemento de esa posición.
+int Size(cola *c);					//Tamaño (Size): Retorna el tamaño de la cola 	
+elemento Element(cola * c, int i);	//Recibe una cola y un número de elemento de 1 al tamaño de la cola y retorna el elemento de esa posición
+void Destroy(cola * c);				//Eliminar cola (Destroy): Recibe una cola y la libera completamente.
+int Search(cola *c, elemento e); 	//Buscar posicion (Search): Recibe una cola y un elemento, devuelve la posicion del
+									//elemento en la cola, o un 0 en caso contrario.
+void Flip(cola *c);					//Voltear cola (Flip): Recibe una cola voltea sus elementos, donde el frente sera el
+									//final, y el final el frente.
