0493fabffaefdad49ed0ce32281661d51db20899
HecRod56 <rodriguez.guarneros.hector.daniel@gmail.com>
2025-12-05 10:08:34 -0600

    Documentación de funciones en planificador.h

--- planificador.h
+++ planificador.h
@@ -21,21 +21,36 @@ OBSERVACIONES:
 #include <sys/types.h>
 #include <unistd.h>
 
-//DEFINICIÓN DE ESTRUCUTRA PARA UN PROCESO
+//DECLARACIÓN DE CONSTANTES
+#define SHM_PATH "/tmp"
+
+//DECLARACIÓN DE ESTRUCUTRA PARA UN PROCESO
 //Se debe considerar que se usara una cola con apuntadores genericos,
 //entonces se debe usar casting para la interacción entre planificador y estructura
 typedef struct Proceso{
     int planificador_pid;
-    int pid;
-
 }Proceso;
 
+//DECLARACIÓN DE ESTRUCTURA PARA LA MEMORIA COMPARTIDAS
+typedef struct SHM_Planificador{
+    int planificador_pid;
+    int pid_proceso;
+    //Semaforos
+}SHM_Planificador;
+
 //DEFINICIÓN DE FUNCIONES PARA PLANIFICADOR
-int inicializar_memoria_compartida();
-int inicializar_semaforos();
-void encolar_proceso(pid_t pid);
+int inicializar_memoria_compartida();   //Inicializar memoria compartida: Crear espacios de memoria 
+                                        //que seran usados por el planificador y procesos
+int inicializar_semaforos();            //Inicializar semaforos: Inicializar semaforos dentro de la memoria
+                                        //compartida para evitar condiciones de carrera y administrar procesos
+void encolar_proceso(pid_t pid);        //Encolar proceso(): Encola la estructura de un proceso 
 Proceso desencolar_proceso();
 void ejecutar_proceso(pid_t pid);
 void detener_proceso(pid_t pid);
 int iniciar_planificador();
 void limpiar();
+
+//DEFINICIÓN DE FUNCIONES PARA PROCESO
+int unirse_memoria_compartida();
+int registrar_proceso();
+int realizar_proceso();
